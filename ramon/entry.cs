6c15484697d2a9b161acd9d91505b3e40c0888c3
RamonFerreira27 <ramon_ferreira27@hotmail.com>
2024-12-18 18:38:36 -0300

    Adding Sort function

--- ramon/entry.cs
+++ ramon/entry.cs
@@ -5,12 +5,17 @@ public class Program
 {
     static void Main(string[] args)
     {
-        List<string> words = ["SEI", "QUE", "VOCÊ", "VAI", "QUERER", "SER", "UMA", "DE", "NÓS!"];
-        List<int> numbers = [1, 2, 3, 4, 1000];
+        List<string> words           = ["SEI", "QUE", "VOCÊ", "VAI", "QUERER", "SER", "UMA", "DE", "NÓS!"];
+        List<int> numbers            = [1, 2, 3, 4];
+        List<int> scrambledNumbers   = [10, 2, 36, 47, 1, 74, 157, 171];
 
-        int resultInt = Find(numbers, target => target == 2);
-        List<string> filteredWords = Filter(words, target => target == "VAI").ToList();
-        List<int> processedNumbers = Map(numbers, target => target * 2).ToList();
+        int resultInt                = Find(numbers, target => target == 2);
+        List<string> filteredWords   = Filter(words, target => target == "VAI").ToList();
+        List<int> processedNumbers   = Map(numbers, target => target * 2).ToList();
+
+        resultInt                    = Reduce(numbers, (left, right) => left * right, 15);
+        words                        = Sort(words).ToList();
+        scrambledNumbers             = Sort(scrambledNumbers).ToList();
     }
 
     private static T? Find<T>(List<T> inputList, Func<T, bool> inputCondition)
@@ -50,4 +55,59 @@ public class Program
 
         return result;
     }
+
+    private static IEnumerable<T> Sort<T>(List<T> inputList)
+    {
+        List<T> resultList = inputList;
+
+        if (resultList.Count <= 1)
+            return resultList;
+
+        int isOrdered = 0;
+
+        do
+        {
+            isOrdered = 0;
+
+            for (int count = 0; count < resultList.Count; count++)
+            {
+                if (count >= resultList.Count - 1)
+                    break;
+
+                string firstValue   = resultList[count].ToString();
+                string secondValue  = resultList[count + 1].ToString();
+
+                for (int innerCount = 0; innerCount < firstValue.Length; innerCount++)
+                {
+                    if (innerCount >= secondValue.Length)
+                    {
+                        isOrdered = ChangePositions(resultList, isOrdered, count);
+                        break;
+                    }
+
+                    if (firstValue[innerCount] == secondValue[innerCount])
+                        continue;
+
+                    if (firstValue[innerCount] > secondValue[innerCount])
+                    {
+                        isOrdered = ChangePositions(resultList, isOrdered, count);
+                    }
+
+                    break;
+                }
+            } 
+        } while (isOrdered != 0);
+
+        return resultList;
+    }
+
+    private static int ChangePositions<T>(List<T> result, int isOrdered, int count)
+    {
+        T aux = result[count + 1];
+        result[count + 1] = result[count];
+        result[count] = aux;
+
+        return ++isOrdered;
+    }
 }
+
